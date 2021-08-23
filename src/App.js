501c610f7090d57ea6bb352ce4365b4dbdfae9d2
wiriux <37052242+wiriux@users.noreply.github.com>
2021-08-23 11:00:57 -0400

    Using components to show display and buttons on screen. As user clicks on the buttons, the correct message is re-rendered and displayed

--- src/App.js
+++ src/App.js
@@ -1,57 +1,41 @@
-// import { checkPropTypes } from 'prop-types'
-import React from 'react'
+import React, {useState} from 'react'
 
-  const Header = (props) =>{
-    return (
-      <div>
-        <p>Title: {props.course.name}</p>
-      </div>
-    )
-  }
+const Display = (props) =>{
+  return (
+    <div>{props.counter}</div>
+  )
+}
 
-  const Content = (props) => {
-    return (
-      <div>
-        <p>Part: {props.parts[0].name}. Exercises: {props.parts[0].exercises}</p>
-        <p>Part: {props.parts[1].name}. Exercises: {props.parts[1].exercises}</p>
-        <p>Part: {props.parts[2].name}. Exercises: {props.parts[2].exercises}</p>
+const Button = (props) =>{
+  return(
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
 
-      </div>
-    )
-  }
+const App = () => {
 
-  const Total = (props) => {
-    return (
-      <div>
-        <p>Total: {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises}</p>
-      </div>
-    )
-  }
+  
+  const [counter, setCounter] = useState(0)
 
-  const App = () =>{
-    const course = {
-      name: 'Half Stack application development',
-        parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero =() => setCounter(0)
+
   return (
     <div>
-      <Header course = {course}/>
-      <Content parts = {course.parts}/>
-      <Total total = {course.parts} />
-
+      <Display counter= {counter}/>
+      <Button onClick= {increaseByOne}
+        text = 'plus'
+      />
+      <Button onClick= {setToZero}
+        text = 'zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+        />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(
- <App />,
- document.getElementById('root') 
-)
+let counter = 1
+
+    ReactDOM.render(
+    <App />,
+    document.getElementById('root') 
+   )
