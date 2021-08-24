e5c3f282f9bd6092c6049186538dcf178f3c0a69
wiriux <37052242+wiriux@users.noreply.github.com>
2021-08-24 10:48:21 -0400

    left and right trigger buttons. Component maintains history of all clicks

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
 import React, {useState} from 'react'
 
-const Display = (props) =>{
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
-    <div>{props.counter}</div>
+    <div>
+      button press history: {props.allClicks.join('')}
+    </div>
+
   )
 }
 
-const Button = (props) =>{
-  return(
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
+const Button = ({handleClick, text}) => (
+  <button onClick = {handleClick}>
+    {text}
+  </button>
+)
+
 
 const App = () => {
 
-  
-  const [counter, setCounter] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero =() => setCounter(0)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-  return (
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return(
     <div>
-      <Display counter= {counter}/>
-      <Button onClick= {increaseByOne}
-        text = 'plus'
-      />
-      <Button onClick= {setToZero}
-        text = 'zero'
-      />
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-        />
+      {left}
+      <Button handleClick = {handleLeftClick} text = 'left'/>
+      <Button handleClick = {handleRightClick} text = 'right'/> 
+      {right}
+      <History allClicks = {allClicks}/>
     </div>
   )
+
 }
 
 export default App
