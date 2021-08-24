6bb3a847ea61b54255eff1a964b026b159f8cf01
wiriux <37052242+wiriux@users.noreply.github.com>
2021-08-24 12:02:02 -0400

    Defining components properly

--- src/App.js
+++ src/App.js
@@ -1,54 +1,30 @@
 import React, {useState} from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0){
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join('')}
-    </div>
+const Display = props => <div>{props.value}</div>
 
-  )
-}
-
-const Button = ({handleClick, text}) => (
-  <button onClick = {handleClick}>
-    {text}
+const Button = (props) => (
+  <button onClick = {props.handleClick}>
+    {props.text}
   </button>
 )
 
-
 const App = () => {
 
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
+ const setToValue = (newValue) => {
+   setValue(newValue)
+ }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
 
-  return(
+  return (
     <div>
-      {left}
-      <Button handleClick = {handleLeftClick} text = 'left'/>
-      <Button handleClick = {handleRightClick} text = 'right'/> 
-      {right}
-      <History allClicks = {allClicks}/>
+      <Display value = {value}/>
+      <button onClick = {() => setToValue(1000)}>thousand</button>
+      <button onClick = {() => setToValue(value + 1)}>increment</button>
+
     </div>
   )
-
 }
 
 export default App
