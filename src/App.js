e41e3e663642761fa81d823095a684817caeb80d
wiriux <37052242+wiriux@users.noreply.github.com>
2021-08-25 10:16:00 -0400

    Allows to receive feedback from customers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import React, {useState} from 'react'
 
+const Display = (props) =>{
+  return (
+    <div>
+      <p>Good: {props.stats[0]} </p>
+      <p>Neutral: {props.stats[1]}</p>
+      <p>Bad: {props.stats[2]}</p>
+
+    </div>
+  )
+}
+
+const Button = (props) =>{
+  return(
+    <button onClick = {props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+
 const App = () => {
   //save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const goodFeedback = () => setGood( good + 1)
+  const neutralFeedback = () => setNeutral(neutral + 1)
+  const negativeFeedback = () => setBad(bad + 1)
+
+
   return (
     <div>
-      
+      <h2>Give Feedback</h2>
+      <Button onClick = {goodFeedback}
+        text = "good"
+      />
+      <Button onClick = {neutralFeedback}
+        text = "neutral"
+      />
+      <Button onClick = {negativeFeedback}
+        text = "bad"
+      />
+      <h2>Statistics</h2>
+      <Display stats = {[good, neutral, bad]}/>
     </div>
   )
 }
