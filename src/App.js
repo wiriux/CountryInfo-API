bbe456d72238e6b76014320b2a03f78611adb26a
wiriux <37052242+wiriux@users.noreply.github.com>
2021-09-07 11:38:09 -0400

    Uses filter to match partial string and displays those users only

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {useState} from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas', phone: '111'},
+    {name: 'John', phone: '222'},
+    {name: 'Daniel', phone: '333'},
+    {name: 'Pamela', phone: '444'}
   ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [patternMatch, setPatternMatch] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -20,6 +24,30 @@ const App = () => {
     }
   }
 
+  const FindPattern = (props) => {
+    let filteredArray = []
+    filteredArray = persons.filter(person => person.name.toLowerCase().includes(props.pattern.toLowerCase()))
+    if(props.pattern === ''){
+     filteredArray = persons
+    }
+    
+    // *** Infinite loop below***
+
+
+    // if (persons.map(person => person.name.includes(props.pattern))) {
+    //   setPatternMatch(patternMatch.concat(props.pattern))
+    //   console.log('test')
+      
+    // }
+    return(
+      <div>
+        {filteredArray.map(name =>
+          <div key={name.name}> {name.name} {name.phone}</div>
+          )} 
+      </div>
+    )
+  }
+
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -30,10 +58,22 @@ const App = () => {
     setNewPhone(event.target.value)
   }
 
+  const handlePatternMatching = (event) => {
+    console.log(event.target.value)
+    setPatternMatch(event.target.value)
+  }
+
+
 
   return(
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with: <input
+          value = {patternMatch}
+          onChange = {handlePatternMatching}/>
+          {/* <FindPattern pattern = {patternMatch}/> */}
+        </div>
       <form onSubmit = {addName}>
         <div>
           name: <input 
@@ -51,9 +91,8 @@ const App = () => {
         <div> debug: {newName}</div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(name =>
-          <div key={name.name}> {name.name} {name.phone}</div>
-          )} 
+      <FindPattern pattern = {patternMatch}/>
+
 
     </div>
   )
