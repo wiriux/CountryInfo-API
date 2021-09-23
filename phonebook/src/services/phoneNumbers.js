222ddb111bb9336ff07c2fcd2c3810721f4fc625
wiriux <37052242+wiriux@users.noreply.github.com>
2021-09-23 10:30:31 -0400

    Updating phonebook project on local directory from repo

---
+++ phonebook/src/services/phoneNumbers.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+const baseURL = 'http://localhost:3001/persons'
+
+const getAll = () =>{
+    const request = axios.get(baseURL)
+    return request.then(response => response.data)
+}
+
+const create = newPerson => {
+    const request = axios.post(baseURL, newPerson)
+    return request.then(response => response.data)
+}
+
+const removePerson = (id) => {
+    const personToRemove = baseURL + '/' + id
+    const request = axios.delete(personToRemove)
+    return request.then(response => response.data)
+}
+
+const updateNumber = (personInfo) => {
+    const [id, person] = personInfo
+    const updateNumber = baseURL + '/' + id
+    const request = axios.put(updateNumber, person)
+    return request.then(response => response.data)
+
+}
+
+const phoneService = {
+   getAll,
+   create,
+   removePerson,
+   updateNumber 
+};
+
+export default phoneService;
