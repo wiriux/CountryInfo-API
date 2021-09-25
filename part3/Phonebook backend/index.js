8bf323114d8400957bfe4b09dca23ef33e531541
wiriux <37052242+wiriux@users.noreply.github.com>
2021-09-25 09:43:37 -0400

    Displays number of people currently in database and time data was requested

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (req, res) =>{
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const numOfPeople = persons.length
+    const date = new Date()
+    res.send(`<div>Phonebook has info for ${numOfPeople} people</div> 
+            <div>${date}</div>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
