7214922ebd7d5958fc87d77fad09938e1a5162e9
wiriux <37052242+wiriux@users.noreply.github.com>
2021-10-08 16:49:32 -0400

    none

--- Notes Frontend/src/services/notes.js
+++ Notes Frontend/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://cryptic-garden-75647.herokuapp.com/api/notes'
 
 const getAll2 = () => {
   const request = axios.get(baseUrl)
